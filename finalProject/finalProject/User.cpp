da59c55e3ce9475f1101b78e4a9d6a6e8aca37c9
NaumovaNatalya2000 <podkina38@gmail.com>
2025-05-08 16:54:24 +0300

    a user class has been created, and the main program menu has been created

---
+++ finalProject/finalProject/User.cpp
@@ -0,0 +1,37 @@
+﻿#include "User.h"
+
+User::User(std::string login, std::string password, std::string name) : _login(login), _password(password), _name(name)
+{
+}
+
+User::~User() = default;
+
+void User::setLogin(const std::string& newLogin)
+{
+	_login = newLogin;
+}
+
+void User::setPassword(const std::string& newPassword)
+{
+	_password = newPassword;
+}
+
+void User::setName(const std::string& newName)
+{
+	_name = newName;
+}
+
+std::string User::getLogin() const
+{
+	return _login;
+}
+
+std::string User::getPassword() const
+{
+	return _password;
+}
+
+std::string User::getName() const
+{
+	return _name;
+}
---
+++ finalProject/finalProject/User.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include<iostream>
+
+class User
+{
+private:
+	std::string _login;
+	std::string _password;
+	std::string _name;
+public:
+	//конструктор по умолчанию
+	User(std::string login, std::string password, std::string name);
+	//деструктор
+	~User();
+	//запрещаем конструкторы копирования и присваивания
+	User(const User&) = delete;
+	User& operator=(const User&) = delete;
+	//сеттеры
+	void setLogin(const std::string& newLogin);
+	void setPassword(const std::string& newPassword);
+	void setName(const std::string& newName);
+	//геттеры
+	std::string getLogin() const;
+	std::string getPassword() const;
+	std::string getName() const;
+};
+
